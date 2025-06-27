538b2f7667629d7a645fa9e5658aad125a2608af
calishu <danny.kretschmann@proton.me>
2025-06-27 16:57:14 +0200

    deletion, cause outdated.

--- hypr/scripts/wallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILE=$(zenity --file-selection --title="Pick your wallpaper" --file-filter='*.jpg *.png *.jpeg *.bmp')
-
-if [[ -n "$FILE" ]]; then
-  hyprctl hyprpaper unload all
-  hyprctl hyprpaper preload "$FILE"
-  hyprctl hyprpaper wallpaper ",$FILE"
-fi
